75e18c2287522000dbc768cbc8ea66ec76f2d0e2
kusuma1605 <kusumakesari@gmail.com>
2026-06-19 13:27:02 +0530

    Delete Module 1 directory

--- Module 1/Javascript/Task 14/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $("#registerBtn").click(function() {
-        $("#eventCard").fadeIn();
-
-        setTimeout(function() {
-            $("#eventCard").fadeOut();
-        }, 3000);
-    });
-});
